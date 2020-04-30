2e0f1f8adf9c549200f15e4e53e5fdc64908657a
Morgan Reilly <morgan.reilly27@gmail.com>
2020-04-30 01:23:22 +0100

    File Reset

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -7,8 +7,6 @@ public class Runner {
 			Each of the languages in the enum Language can be represented as a number between 0 and 234. You can 
 			map the output of the neural network and the training data label to / from the language using the
 			following. Eg. index 0 maps to Achinese, i.e. langs[0].  
-			
-			Training data? 
 		*/
 		Language[] langs = Language.values(); //Only call this once...		
 		for (int i = 0; i < langs.length; i++){
